017368e6e74d9a0111f2248750c9d616e6b23ed5
Ishwarpal <ishwarpal.gill@gmail.com>
2020-01-13 17:51:13 -0500

    First Commit for Bonus lab 9

---
+++ Bonus Lab 9/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bonus_Lab_9.Models;
+
+namespace Bonus_Lab_9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //by adding a new action, i can load a new view
+        public IActionResult About()
+        {
+            //return View here, if view is not specified it defualts to the action name
+            return View();
+        }
+
+        //add a portfolio action
+        public IActionResult Portfolio()
+        {
+            //return portfolio action
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bonus Lab 9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bonus_Lab_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
